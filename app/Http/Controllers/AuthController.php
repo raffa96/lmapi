fba3cfaf9ba233438fa5f790072702607f3f407e
Raffaele Tufano <raffaele@thebubblecompany.com>
2024-12-13 18:02:16 +0100

    Initial commit.

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Traits\ApiResponses;
+use Illuminate\Http\JsonResponse;
+
+class AuthController extends Controller
+{
+    use ApiResponses;
+
+    public function login(): JsonResponse
+    {
+        return $this->ok('Hello, Login!');
+    }
+}
