0bbe2038abea1a1d9e9cc22b254e90eeef367cca
Raffaele Tufano <raffaele@thebubblecompany.com>
2025-01-15 15:14:18 +0100

    develop: implementing filters api system.

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -2,17 +2,25 @@
 
 namespace App\Models;
 
+use App\Http\Filters\V1\QueryFilter;
+use Database\Factories\TicketFactory;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Ticket extends Model
 {
-    /** @use HasFactory<\Database\Factories\TicketFactory> */
+    /** @use HasFactory<TicketFactory> */
     use HasFactory;
 
-    public function user(): BelongsTo
+    public function author(): BelongsTo
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function scopeFilter(Builder $builder, QueryFilter $filters): Builder
+    {
+        return $filters->apply($builder);
     }
 }
